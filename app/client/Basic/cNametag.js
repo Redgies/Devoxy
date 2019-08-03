37461522e1a211cbe3f09781b8d6f01f131e7d72
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-04 00:50:59 +0200

    Update cNametag.js

--- app/client/Basic/cNametag.js
+++ app/client/Basic/cNametag.js
@@ -12,6 +12,8 @@ mp.events.add('render', (nametags) => {
 	
     nametags.forEach(nametag => {
         let [player, x, y, distance] = nametag;
+
+        console.log(player);
 		
         if(distance <= maxDistance) {	   
             let scale = (distance / maxDistance);
