c58642fe86685f05ad18acefad396a13de8e888d
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-04 00:31:57 +0200

    test
    test

--- app/server/Basic/sPlayer.js
+++ app/server/Basic/sPlayer.js
@@ -188,29 +188,29 @@ function playerExitVehicleHandler(player) {
 
     mp.events.add("playerExitVehicle", playerExitVehicleHandler);
 
-const maxDistance = 25*25;
-const width = 0.03;
-const height = 0.0065;﻿
-const border = 0.001;
-const color = [255,255,255,255];
+// const maxDistance = 25*25;
+// const width = 0.03;
+// const height = 0.0065;﻿
+// const border = 0.001;
+// const color = [255,255,255,255];
 
-// mp.nametags.enabled = false;
+// // mp.nametags.enabled = false;
 
-mp.events.add('render', (nametags) => {
-    const graphics = mp.game.graphics;
-    const screenRes = graphics.getScreenResolution(0, 0);
+// mp.events.add('render', (nametags) => {
+//     const graphics = mp.game.graphics;
+//     const screenRes = graphics.getScreenResolution(0, 0);
     
-    nametags.forEach(nametag => {
-        let [player, x, y, distance] = nametag;
+//     nametags.forEach(nametag => {
+//         let [player, x, y, distance] = nametag;
         
-        if(distance <= maxDistance) {	   
-            let scale = (distance / maxDistance);
-            if(scale < 0.6) scale = 0.6;
+//         if(distance <= maxDistance) {	   
+//             let scale = (distance / maxDistance);
+//             if(scale < 0.6) scale = 0.6;
             
-            y -= scale * (0.005 * (screenRes.y / 1080));
+//             y -= scale * (0.005 * (screenRes.y / 1080));
         
-            graphics.drawText("NOUVEAU JOUEUR", 4, color, 0.4, 0.4, true, x, y);
-        }
-    });
-});
+//             graphics.drawText("NOUVEAU JOUEUR", 4, color, 0.4, 0.4, true, x, y);
+//         }
+//     });
+// });
 
