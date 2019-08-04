8b7f2144e96b804f197206135790f533da7a9269
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-04 23:36:32 +0200

    ajout cef moto

--- app/client/Business/cCarDealership.js
+++ app/client/Business/cCarDealership.js
@@ -11,5 +11,11 @@ mp.events.add({
 		misc.prepareToCef(500);
 		misc.openCef("package://RP/Browsers/Business/CommercialCarDealership/ccd.html", lang);
 		misc.injectCef(inject);
+	},
+
+	"cCheapCarDealership-OpenBuyerMenu" : (lang, inject) => {
+		misc.prepareToCef(500);
+		misc.openCef("package://RP/Browsers/Business/MotoDealership/ccd.html", lang);
+		misc.injectCef(inject);
 	}
 });
