f748477d180e6bb92ae4eae395cc1abd85096cb5
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-05 16:47:16 +0200

    Update sPhone.js

--- app/server/Phone/sPhone.js
+++ app/server/Phone/sPhone.js
@@ -5,25 +5,25 @@ class Phone {
         mp.events.add({
             "sKeys-F6" : (player) => {
                 if(!player.loggedIn) return;
-                let execute = `app.playerMessages('${this.getMessages()}');`;
+                let execute = `app.playerMessages('${this.getMessages(player)}');`;
 
                 player.call("cPhone-Open", [execute]);
-                misc.log.debug(`${player.name} opens tab`);
+                misc.log.debug(`${player.name} opens phone`);
             }
         });
 
         mp.events.addCommand({
 			'phone' : (player, fullText) => {
                 if(!player.loggedIn) return;
-                let execute = `app.playerMessages('${this.getMessages()}');`;
+                let execute = `app.playerMessages('${this.getMessages(player)}');`;
 
                 player.call("cPhone-Open", [execute]);
-                misc.log.debug(`${player.name} opens tab`);
+                misc.log.debug(`${player.name} opens phone`);
             }
         });
     }
 
-    getMessages() {
+    getMessages(player) {
         const messagesList = [];
         
         const d = misc.query(`SELECT * FROM phoneMessages WHERE receiver = '${player.number}'`);
