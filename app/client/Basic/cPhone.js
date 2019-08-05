3349a748a35850dbcb79aeb1681a0e620264e487
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-05 14:01:25 +0200

    Update cPhone.js

--- app/client/Basic/cPhone.js
+++ app/client/Basic/cPhone.js
@@ -2,7 +2,7 @@ const misc = require('../cMisc');
 
 mp.events.add({
 	"cPhone-Open" : (inject) => {
-		misc.prepareToCef(1);
+		misc.prepareToCef(0);
 		misc.openCef("package://RP/Browsers/Phone/phone.html");
 		misc.injectCef(inject);
 	},
