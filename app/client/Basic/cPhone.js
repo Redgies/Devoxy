9385e5f6c7adf88ddebaffe5895e992129d066c6
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-06 03:33:00 +0200

    cc

--- app/client/Basic/cPhone.js
+++ app/client/Basic/cPhone.js
@@ -4,7 +4,10 @@ mp.events.add({
 	"cPhone-Open" : (inject) => {
 		misc.prepareToCef(0);
 		misc.openCef("package://RP/Browsers/Phone/phone.html");
-		misc.injectCef(inject, false);
+		misc.injectCef(inject);
 	},
+	"cPhone-Update" : (inject) => {
+		misc.injectCef(inject);
+	}
 	
 });
