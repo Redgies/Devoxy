d5c5b82ef1e3ff1d74f807ab7963bee56000afc8
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-08 01:42:42 +0200

    Update sPolice.js

--- app/server/Factions/sPolice.js
+++ app/server/Factions/sPolice.js
@@ -37,10 +37,18 @@ class Police extends faction {
             player.setClothes(4, 35, 0, 2);
             player.setClothes(11, 55, 0, 2);
         }
-        if(player.rank == 2 || player.rank == 3)
+        if(player.rank == 2)
         {
             player.setProp(0, 46, 0);
             player.setClothes(8, 58, 0, 2); 
+            player.setClothes(6, 25, 0, 2);
+            player.setClothes(4, 35, 0, 2);
+            player.setClothes(11, 55, 0, 2);
+        }
+        if(player.rank == 3)
+        {
+            player.setProp(0, 46, 0);
+            player.setClothes(8, 128, 0, 2); 
             player.setClothes(6, 25, 0, 2);
             player.setClothes(4, 35, 0, 2);
             player.setClothes(11, 55, 0, 2);
@@ -63,24 +71,26 @@ class Police extends faction {
             player.setClothes(11, 43, 0, 2);
             player.setClothes(3, 11, 0, 2);
         }
-        if(player.rank == 5)
-        {
-            player.setProp(0, 46, 0);
-            player.setClothes(8, 58, 0, 2); 
-            player.setClothes(6, 25, 0, 2);
-            player.setClothes(4, 35, 0, 2);
-            player.setClothes(11, 43, 0, 2);
-            player.setClothes(3, 11, 0, 2);
-        }
         if(player.rank == 6)
         {
             // player.setProp(0, 46, 0);
-            player.setClothes(8, 58, 0, 2); 
+            // player.setClothes(8, 58, 0, 2); 
             player.setClothes(6, 25, 0, 2);
             player.setClothes(4, 35, 0, 2);
             player.setClothes(11, 26, 0, 2);
             player.setClothes(3, 11, 0, 2);
-            // player.setClothes(7, 125, 0, 2);
+            player.setClothes(7, 125, 0, 2);
+        }
+        if(player.rank == 6)
+        {
+            // player.setProp(0, 46, 0);
+            // player.setClothes(8, 58, 0, 2); 
+            player.setClothes(6, 25, 0, 2);
+            player.setClothes(4, 35, 0, 2);
+            player.setClothes(11, 26, 0, 2);
+            player.setClothes(3, 11, 0, 2);
+            player.setClothes(7, 125, 0, 2);
+            player.setClothes(8, 131, 0, 2);
         }
 	}
 
