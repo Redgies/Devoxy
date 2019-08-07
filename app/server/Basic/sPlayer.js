4e62e8ea5917cbbe3af36cf60b4607e523e77d03
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-07 21:33:10 +0200

    d

--- app/server/Basic/sPlayer.js
+++ app/server/Basic/sPlayer.js
@@ -49,7 +49,7 @@ class PlayerSingleton {
         const q3 = clothesSingleton.loadPlayerClothes(player);
         const q4 = headOverlaySingleton.loadUser(player);
         const q5 = vehiclesSingleton.loadPlayerVehicles(player.guid);
-        const q6 = faction.loadUser(player);
+        // const q6 = faction.loadUser(player);
         const q7 = hospital.loadUser(player);
         const q8 = prison.loadUser(player);
         await Promise.all([q1, q2, q3, q4, q5, q6, q7, q8]);
