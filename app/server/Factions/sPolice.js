1753af82efa2aee27326e60c75e47a85a0a56fd3
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-08 01:02:47 +0200

    Update sPolice.js

--- app/server/Factions/sPolice.js
+++ app/server/Factions/sPolice.js
@@ -54,7 +54,7 @@ class Police extends faction {
             player.setClothes(6, 25, 0, 2);
             player.setClothes(4, 35, 0, 2);
             player.setClothes(11, 55, 0, 2);
-            player.setClothes(10, 8, 0, 2);
+            player.setClothes(10, 8, 1, 2);
         }
 	}
 
