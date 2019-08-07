67f53212a8101cb38341a966523f53a2b0afd1bd
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-08 01:31:48 +0200

    Update sPolice.js

--- app/server/Factions/sPolice.js
+++ app/server/Factions/sPolice.js
@@ -62,7 +62,25 @@ class Police extends faction {
             player.setClothes(4, 35, 0, 2);
             player.setClothes(11, 43, 0, 2);
             player.setClothes(3, 11, 0, 2);
-            // player.setClothes(10, 8, 1, 2);
+        }
+        if(player.rank == 5)
+        {
+            player.setProp(0, 46, 0);
+            player.setClothes(8, 58, 0, 2); 
+            player.setClothes(6, 25, 0, 2);
+            player.setClothes(4, 35, 0, 2);
+            player.setClothes(11, 43, 0, 2);
+            player.setClothes(3, 11, 0, 2);
+        }
+        if(player.rank == 6)
+        {
+            player.setProp(0, 46, 0);
+            player.setClothes(8, 58, 0, 2); 
+            player.setClothes(6, 25, 0, 2);
+            player.setClothes(4, 35, 0, 2);
+            player.setClothes(11, 43, 0, 2);
+            player.setClothes(3, 11, 0, 2);
+            player.setClothes(7, 125, 0, 2);
         }
 	}
 
