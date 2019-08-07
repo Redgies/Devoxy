ab1f4aaf6d40723062b422a0abbab6821571407b
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-07 20:35:05 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname),
-        filename: '[name]/sNoClip.js'
+        filename: '[name]/index.js'
     },
     target: 'node', // in order to ignore built-in modules like path, fs, etc.
     externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
