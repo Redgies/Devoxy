210a5e2e73c99e8a1ba410f836a35bbccbbf26e5
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-08 15:19:57 +0200

    Update cInterface.js

--- app/client/Basic/cInterface.js
+++ app/client/Basic/cInterface.js
@@ -2,7 +2,7 @@ const misc = require('../cMisc');
 
 mp.events.add({
 	"cInterface-Open" : (inject) => {
-		misc.prepareToCef(0, false, true, true);
+		misc.prepareToCef(0, true, true, true);
 		misc.openCef("package://RP/Browsers/Interface/interface.html");
 		misc.injectCef(inject);
 	},
