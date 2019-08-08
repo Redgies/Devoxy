ae75fcf19f59f5751fec1ce9b00535a67765c9f5
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-08 19:31:54 +0200

    Update sInterface.js

--- app/server/Basic/sInterface.js
+++ app/server/Basic/sInterface.js
@@ -6,8 +6,8 @@ class Interface {
             "sKeys-F3" : (player) => {
                 if(!player.loggedIn) return;
 
-                // let target = misc.getNearestPlayer(player, 1);
-                let target = player;
+                let target = misc.getNearestPlayer(player, 1);
+                // let target = player;
                 let veh = misc.getNearestVehicle(player, 3);
 
                 if(target)
