6bb4c547d5012f65cee6099e323c59375ab59dd1
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-08 17:35:10 +0200

    Update sInterface.js

--- app/server/Basic/sInterface.js
+++ app/server/Basic/sInterface.js
@@ -21,13 +21,15 @@ class Interface {
                 misc.log.debug(`${player.name} opens interface`);
             },
             "sInterface-giveMoney": (player, money) => {
-                console.log("sInterface-giveMoney id : " + player.target.id + " money : " + money);
-                let execute = `app.whoName = '${player.name}';`;
-                execute += `app.whoId = ${player.target.id};`;
-                execute += `app.wantText = 'Veux te donner de l'oseil kwa;`;
-                execute += `app.priceText = 'nike ta mère'`;
-                execute += `app.price = ${money};`;
-                player.target.call("cMisc-CreateChooseWindow", [player.lang, execute, "test", "test"]);
+                player.target.money.cash += parseInt(money);
+                player.money.cash -= parseInt(money);
+                // console.log("sInterface-giveMoney id : " + player.target.id + " money : " + money);
+                // let execute = `app.whoName = '${player.name}';`;
+                // execute += `app.whoId = ${player.target.id};`;
+                // execute += `app.wantText = 'Veux te donner de l'oseil kwa;`;
+                // execute += `app.priceText = 'nike ta mère'`;
+                // execute += `app.price = ${money};`;
+                // player.target.call("cMisc-CreateChooseWindow", [player.lang, execute, "test", "test"]);
         
                 // const d = JSON.parse(str);
 
