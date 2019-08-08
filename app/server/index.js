3734736272b7f01c7fe467883494acea386ae461
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-08 03:08:50 +0200

    d

--- app/server/index.js
+++ app/server/index.js
@@ -15,6 +15,7 @@ require('./Jobs/sOrangeCollector');
 require('./Jobs/sCluckinBellCourier');
 require('./Factions/sPolice');
 require('./Factions/sGarage');
+require('./Factions/sBuilding');
 require('./Basic/sMenu');
 require('./Basic/sTab');
 require('./Phone/sPhone');
