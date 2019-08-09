317e352d74a0e6f8aff644d6f0830371c00431a8
Riotor Mathias <mathias.riotor@gmail.com>
2019-08-09 19:54:11 +0200

    car tuner

--- app/server/CarTuner/index.js
+++ app/server/CarTuner/index.js
@@ -1,9 +1,13 @@
+mp.events.add({
+    "sKeys-F3": (player) => {
+        player.call("cTuner-Open", []);
+    }
+});
 
-// Tuning
 mp.events.add("mod", (player, mod, modvalue) => {
     player.vehicle.setMod(parseInt(mod), parseInt(modvalue));
 });
 
-mp.events.addCommand('mod', (player, fullText, a , b) => {
+mp.events.addCommand('mod', (player, fullText, a, b) => {
     player.vehicle.setMod(parseInt(a), parseInt(b));
-  });
+});
