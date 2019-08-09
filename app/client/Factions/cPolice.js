1dc522639b115bb6d1ce959ffa8b6e8ed21e905f
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-09 16:28:33 +0200

    Update cPolice.js

--- app/client/Factions/cPolice.js
+++ app/client/Factions/cPolice.js
@@ -6,12 +6,14 @@ player = mp.players.local;
 mp.events.add(
 {
 	"cCuff": () => {        
+		mp.game.graphics.notify('~g~Hello ~w~World');
         player.setEnableHandcuffs(true);
 		player.cuffed = true;
 		mp.game.controls.disableControlAction(0, 22, true);
 		mp.game.controls.disableControlAction(1, 22, true);
 		mp.game.controls.disableControlAction(2, 22, true);
 		mp.game.controls.disableControlAction(32, 22, true);
+		mp.game.controls.disableControlAction(2, 15, true);
     },
     "cUnCuff": () => {
         player.setEnableHandcuffs(false);
