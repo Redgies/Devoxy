f38a865d0c8631ba96134d446f9420ba067d4cd3
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-09 16:43:49 +0200

    Update cPolice.js

--- app/client/Factions/cPolice.js
+++ app/client/Factions/cPolice.js
@@ -9,41 +9,24 @@ mp.events.add(
 		mp.game.graphics.notify('~g~Hello ~w~World');
         player.setEnableHandcuffs(true);
 		player.cuffed = true;
-		mp.game.controls.disableControlAction(0, 21, true);
-		mp.game.controls.disableControlAction(1, 21, true);
-		mp.game.controls.disableControlAction(2, 21, true);
-		mp.game.controls.disableControlAction(3, 21, true);
-		mp.game.controls.disableControlAction(4, 21, true);
-		mp.game.controls.disableControlAction(5, 21, true);
-		mp.game.controls.disableControlAction(6, 21, true);
-		mp.game.controls.disableControlAction(7, 21, true);
-		mp.game.controls.disableControlAction(8, 21, true);
-		mp.game.controls.disableControlAction(9, 21, true);
-		mp.game.controls.disableControlAction(10, 21, true);
-		mp.game.controls.disableControlAction(11, 21, true);
-		mp.game.controls.disableControlAction(12, 21, true);
-		mp.game.controls.disableControlAction(13, 21, true);
-		mp.game.controls.disableControlAction(14, 21, true);
-		mp.game.controls.disableControlAction(15, 21, true);
-		mp.game.controls.disableControlAction(16, 21, true);
-		mp.game.controls.disableControlAction(17, 21, true);
-		mp.game.controls.disableControlAction(18, 21, true);
-		mp.game.controls.disableControlAction(19, 21, true);
-		mp.game.controls.disableControlAction(20, 21, true);
-		mp.game.controls.disableControlAction(21, 21, true);
-		mp.game.controls.disableControlAction(22, 21, true);
-		mp.game.controls.disableControlAction(23, 21, true);
-		mp.game.controls.disableControlAction(24, 21, true);
-		mp.game.controls.disableControlAction(25, 21, true);
-		mp.game.controls.disableControlAction(26, 21, true);
-		mp.game.controls.disableControlAction(27, 21, true);
-		mp.game.controls.disableControlAction(28, 21, true);
-		mp.game.controls.disableControlAction(29, 21, true);
-		mp.game.controls.disableControlAction(30, 21, true);
-		mp.game.controls.disableControlAction(31, 21, true);
-		mp.game.controls.disableControlAction(32, 21, true);
-		mp.game.controls.disableControlAction(33, 21, true);
-		mp.game.controls.disableControlAction(2, 19, true);
+		mp.game.controls.disableControlAction(0, 21, true); // disable sprint
+        mp.game.controls.disableControlAction(0, 24, true); // disable attack
+        mp.game.controls.disableControlAction(0, 25, true); // disable aim
+        mp.game.controls.disableControlAction(0, 47, true); // disable weapon
+        mp.game.controls.disableControlAction(0, 58, true); // disable weapon
+        mp.game.controls.disableControlAction(0, 263, true); // disable melee
+        mp.game.controls.disableControlAction(0, 264, true); // disable melee
+        mp.game.controls.disableControlAction(0, 257, true); // disable melee
+        mp.game.controls.disableControlAction(0, 140, true); // disable melee
+        mp.game.controls.disableControlAction(0, 141, true); // disable melee
+        mp.game.controls.disableControlAction(0, 142, true); // disable melee
+        mp.game.controls.disableControlAction(0, 143, true); // disable melee
+        mp.game.controls.disableControlAction(0, 75, true); // disable exit vehicle
+        mp.game.controls.disableControlAction(27, 75, true); // disable exit vehicle
+        mp.game.controls.disableControlAction(0, 32, true); // move (w)
+        mp.game.controls.disableControlAction(0, 34, true); // move (a)
+        mp.game.controls.disableControlAction(0, 33, true); // move (s)
+        mp.game.controls.disableControlAction(0, 35, true); // move (d)
     },
     "cUnCuff": () => {
         player.setEnableHandcuffs(false);
