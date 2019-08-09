d6fa78a743e3396f3c2b168b3c4ea517c0623c66
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-09 14:35:08 +0200

    Update cScaleForm.js

--- app/client/ScaleForm/cScaleForm.js
+++ app/client/ScaleForm/cScaleForm.js
@@ -1,6 +1,6 @@
-var messageScaleform = require("./ScaleForm/Scaleform.js");
-require("./ScaleForm/BigMessage.js");
-require("./ScaleForm/MidsizedMessage.js");
+var messageScaleform = require("./Scaleform.js");
+require("./BigMessage.js");
+require("./MidsizedMessage.js");
 
 mp.game.ui.messages = {
     showShard: (title, message, titleColor, bgColor, time = 5000) => mp.events.call("ShowShardMessage", title, message, titleColor, bgColor, time),
