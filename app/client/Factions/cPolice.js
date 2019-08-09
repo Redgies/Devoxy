d7492ae2557c3989e388b6574336c09640e57bb1
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-09 16:51:47 +0200

    Update cPolice.js

--- app/client/Factions/cPolice.js
+++ app/client/Factions/cPolice.js
@@ -17,6 +17,7 @@ mp.events.add(
 	"render": () => {
 		if(player.cuffed)
 		{
+			mp.game.controls.disableControlAction(0, 22, true); // disable jump
 			mp.game.controls.disableControlAction(0, 21, true); // disable sprint
 			mp.game.controls.disableControlAction(0, 24, true); // disable attack
 			mp.game.controls.disableControlAction(0, 25, true); // disable aim
@@ -29,12 +30,13 @@ mp.events.add(
 			mp.game.controls.disableControlAction(0, 141, true); // disable melee
 			mp.game.controls.disableControlAction(0, 142, true); // disable melee
 			mp.game.controls.disableControlAction(0, 143, true); // disable melee
-			mp.game.controls.disableControlAction(0, 75, true); // disable exit vehicle
-			mp.game.controls.disableControlAction(27, 75, true); // disable exit vehicle
+			// mp.game.controls.disableControlAction(0, 75, true); // disable exit vehicle
+			// mp.game.controls.disableControlAction(27, 75, true); // disable exit vehicle
 			mp.game.controls.disableControlAction(0, 32, true); // move (w)
 			mp.game.controls.disableControlAction(0, 34, true); // move (a)
 			mp.game.controls.disableControlAction(0, 33, true); // move (s)
 			mp.game.controls.disableControlAction(0, 35, true); // move (d)
+			mp.game.controls.disableControlAction(0, 37, true);
 		}
 	}
 
