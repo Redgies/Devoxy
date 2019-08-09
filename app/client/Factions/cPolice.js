d659dc439e0909cfd370da96acc49078d27a96aa
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-09 16:45:26 +0200

    Update cPolice.js

--- app/client/Factions/cPolice.js
+++ app/client/Factions/cPolice.js
@@ -9,28 +9,33 @@ mp.events.add(
 		mp.game.graphics.notify('~g~Hello ~w~World');
         player.setEnableHandcuffs(true);
 		player.cuffed = true;
-		mp.game.controls.disableControlAction(0, 21, true); // disable sprint
-        mp.game.controls.disableControlAction(0, 24, true); // disable attack
-        mp.game.controls.disableControlAction(0, 25, true); // disable aim
-        mp.game.controls.disableControlAction(0, 47, true); // disable weapon
-        mp.game.controls.disableControlAction(0, 58, true); // disable weapon
-        mp.game.controls.disableControlAction(0, 263, true); // disable melee
-        mp.game.controls.disableControlAction(0, 264, true); // disable melee
-        mp.game.controls.disableControlAction(0, 257, true); // disable melee
-        mp.game.controls.disableControlAction(0, 140, true); // disable melee
-        mp.game.controls.disableControlAction(0, 141, true); // disable melee
-        mp.game.controls.disableControlAction(0, 142, true); // disable melee
-        mp.game.controls.disableControlAction(0, 143, true); // disable melee
-        mp.game.controls.disableControlAction(0, 75, true); // disable exit vehicle
-        mp.game.controls.disableControlAction(27, 75, true); // disable exit vehicle
-        mp.game.controls.disableControlAction(0, 32, true); // move (w)
-        mp.game.controls.disableControlAction(0, 34, true); // move (a)
-        mp.game.controls.disableControlAction(0, 33, true); // move (s)
-        mp.game.controls.disableControlAction(0, 35, true); // move (d)
     },
     "cUnCuff": () => {
         player.setEnableHandcuffs(false);
         player.cuffed = false;
-    },
+	},
+	"render": () => {
+		if(player.cuffed)
+		{
+			mp.game.controls.disableControlAction(0, 21, true); // disable sprint
+			mp.game.controls.disableControlAction(0, 24, true); // disable attack
+			mp.game.controls.disableControlAction(0, 25, true); // disable aim
+			mp.game.controls.disableControlAction(0, 47, true); // disable weapon
+			mp.game.controls.disableControlAction(0, 58, true); // disable weapon
+			mp.game.controls.disableControlAction(0, 263, true); // disable melee
+			mp.game.controls.disableControlAction(0, 264, true); // disable melee
+			mp.game.controls.disableControlAction(0, 257, true); // disable melee
+			mp.game.controls.disableControlAction(0, 140, true); // disable melee
+			mp.game.controls.disableControlAction(0, 141, true); // disable melee
+			mp.game.controls.disableControlAction(0, 142, true); // disable melee
+			mp.game.controls.disableControlAction(0, 143, true); // disable melee
+			mp.game.controls.disableControlAction(0, 75, true); // disable exit vehicle
+			mp.game.controls.disableControlAction(27, 75, true); // disable exit vehicle
+			mp.game.controls.disableControlAction(0, 32, true); // move (w)
+			mp.game.controls.disableControlAction(0, 34, true); // move (a)
+			mp.game.controls.disableControlAction(0, 33, true); // move (s)
+			mp.game.controls.disableControlAction(0, 35, true); // move (d)
+		}
+	}
 
 });
