661974bf3b0c0293071f2f6225d97a7fc2e6a49f
Riotor Mathias <mathias.riotor@gmail.com>
2019-08-10 14:26:28 +0200

    blblbl

--- app/server/Basic/sInterface.js
+++ app/server/Basic/sInterface.js
@@ -51,12 +51,8 @@ class Interface {
             },
             "sInterface-setTunning": (player, mod, modvalue) => {
                 let veh = misc.getNearestVehicle(player, 3);
-                if (mod === parseInt(46)) {
-                    veh.windowTint(parseInt(modvalue));
-                } else {
+                veh.setMod(parseInt(mod), parseInt(modvalue));
 
-                    veh.setMod(parseInt(mod), parseInt(modvalue));
-                }
             },
             "sInterface-setCuff": (player, data) => {
                 const d = JSON.parse(data);
