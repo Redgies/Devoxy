325b3c5b434268b1c22ffb9cbe684c5550093e82
Riotor Mathias <mathias.riotor@gmail.com>
2019-08-10 10:46:06 +0200

    fgqsfsq

--- app/server/Basic/sDoors.js
+++ app/server/Basic/sDoors.js
@@ -11,6 +11,8 @@ class Doors {
             {id: 5, model: 631614199, x: 461.8065, y: -994.4086, z: 25.06443, open: false, faction: 1 }, // COMICO CELLULE 1
             {id: 6, model: 631614199, x: 461.8065, y: -997.6583, z: 25.06443, open: false, faction: 1 }, // COMICO CELLULE 2
             {id: 7, model: 631614199, x: 461.8065, y: -1001.302, z: 25.06443, open: false, faction: 1 }, // COMICO CELLULE 3
+            {id: 8, model: -550347177, x: -356.0905, y: -134.7714, z: 40.01295, open: true, faction: 2 }, // LOS SANTOS CUSTOM 1
+            {id: 9, model: -550347177, x: -1145.898, y: -1991.144, z: 14.18357, open: true, faction: 2 }, // LOS SANTOS CUSTOM
         ];
 
         this.createShapes();
