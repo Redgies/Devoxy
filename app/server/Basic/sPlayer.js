ee31c5ed9ba4682e8ddc1cdd624ff56ecd793798
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-11 11:24:03 +0200

    Update sPlayer.js

--- app/server/Basic/sPlayer.js
+++ app/server/Basic/sPlayer.js
@@ -50,7 +50,7 @@ class PlayerSingleton {
 
         for(let i = 0; i < player.pWeapons.length; i++)
         {
-            console.log(player.pWeapons[i]);
+            console.log("hash : " + player.pWeapons[i].hash = " ammo : " + player.pWeapons[i].ammo);
             player.giveWeapon(player.pWeapons[i].hash, player.pWeapons[i].ammo);
         }
 
