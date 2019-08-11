9b3ad660187715381c33c7f8c4a9da954c79a0de
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-11 12:25:59 +0200

    Update cPlayer.js

--- app/client/Basic/cPlayer.js
+++ app/client/Basic/cPlayer.js
@@ -1,9 +1,14 @@
 const misc = require('../cMisc');
 
+let player = mp.players.local;
+
 mp.events.add({
 	"cPlayer-getWeaponAmmo" : () => {
-        let ammo = mp.players.local.getWeaponAmmo();
+        var weapon_hash = player.weapon;
 
-        mp.gui.chat.push("ammo : " + ammo);
+        let ammoClip = player.getAmmoInClip(weapon_hash);
+        let ammo = player.getWeaponAmmo();
+
+        mp.gui.chat.push("ammo : " + ammo + " - " + ammoClip);
     },
 });
