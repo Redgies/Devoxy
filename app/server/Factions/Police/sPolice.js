c4c4c20ad6b2e65c249f779f253748e72c5ee5db
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-12 00:56:35 +0200

    Update sPolice.js

--- app/server/Factions/Police/sPolice.js
+++ app/server/Factions/Police/sPolice.js
@@ -84,7 +84,7 @@ class Police extends faction {
 				if(!target)
                     return player.notify("~r~Cette personne n'est pas connecté.");
 
-                let message = fullCmd.substr(arg1.length + 1, fullCmd.length)
+                let message = fullText.substr(arg1.length + 1, fullText.length);
 
                 // if(message.length <= 0)
                 //     return player.notify("~r~Utilisez /delit id message");
