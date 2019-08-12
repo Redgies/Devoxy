66eb48fae3961b202e7438e4566b4a1b8edccdb4
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-12 13:57:53 +0200

    Update sLocation.js

--- app/server/Basic/sLocation.js
+++ app/server/Basic/sLocation.js
@@ -1,5 +1,5 @@
 const misc = require('../sMisc');
-const Vehicle = require('./sVehicle');
+const Vehicle = require('./Vehicles/sVehicle');
 
 const locationsData = [
     {pos: {x:-1038.748, y: -2731.016, z: 20.169}} // AEROPORT
