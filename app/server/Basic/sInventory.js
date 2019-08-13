c50297bcd32e06dbf58bfa9abf1f25c602030bca
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-13 17:57:50 +0200

    Update sInventory.js

--- app/server/Basic/sInventory.js
+++ app/server/Basic/sInventory.js
@@ -1,1 +1,46 @@
 const invAPI = require("../3rd/inventory.js");
+
+invAPI.on("itemDefined", (key, name, description) => {
+    console.log(`Item defined, key: ${key} | name: ${name} | description: ${description}`);
+});
+
+invAPI.on("itemAdded", (player, key, amount, data) => {
+    console.log(`${player.name} received ${amount}x ${key}.`);
+});
+
+invAPI.on("itemUsed", (player, invIdx, key, data) => {
+    console.log(`${player.name} used ${key}.`);
+});
+
+invAPI.on("itemRemoved", (player, invIdx, key, amount, data) => {
+    console.log(`${player.name} lost ${amount}x ${key}.`);
+});
+
+invAPI.on("itemRemovedCompletely", (player, key, data) => {
+    console.log(`${player.name} no longer has ${key} (${data ? "with data" : "without data"}) in their inventory.`);
+});
+
+invAPI.on("inventoryReplaced", (player, oldInventory, newInventory) => {
+    console.log(`${player.name} had their inventory replaced. (Old item count: ${oldInventory.length}, new: ${newInventory.length})`);
+});
+
+mp.events.addCommand("givehoodie", (player, _, texture) => {
+    const giveItemResult = player.giveItem("item_male_hoodie", 1, {
+        texture: Number(texture)
+    });
+
+    if (giveItemResult) {
+        player.outputChatBox(`Received a male hoodie with texture variation ${texture}.`);
+    } else {
+        player.outputChatBox("Failed to give item.");
+    }
+});
+
+mp.events.addCommand("inventory", (player) => {
+    const inventory = player.getInventory();
+
+    player.outputChatBox("Your inventory:");
+    inventory.forEach((item, index) => {
+        player.outputChatBox(`${index} | ${invAPI.getItemName(item.key)} (${item.key}) | ${item.amount}x`);
+    });
+});
