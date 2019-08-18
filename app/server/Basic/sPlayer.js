a6ed93abefb3751543b2a601d27a3e0d044edd7d
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-19 01:11:44 +0200

    Update sPlayer.js

--- app/server/Basic/sPlayer.js
+++ app/server/Basic/sPlayer.js
@@ -69,7 +69,7 @@ class PlayerSingleton {
         else 
             player.tp(JSON.parse(d[0].position));
 
-        player.inCall = -1;
+        player.inCall = 0;
         player.setVariable("adminLvl", player.adminLvl);
         player.call("createPed");
         player.call("cCloseCefAndDestroyCam");
