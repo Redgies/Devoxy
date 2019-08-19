8b000ae111137ab9d5da6ec64ccb2b5fff8ac4c6
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-19 12:59:42 +0200

    Update sVip.js

--- app/server/Basic/sVip.js
+++ app/server/Basic/sVip.js
@@ -1,5 +1,20 @@
 const misc = require('../sMisc');
 
+async function tryVipCode(player, code) {
+    player.outputChatBox("test 2 : " + code);
+
+    const d = await misc.query(`SELECT payment_status, payment_type, payment_code_used FROM paiements WHERE payment_code = '${code}' LIMIT 1`);
+    if (!d[0]) {
+        return this.showError(player, "Ce code n'éxiste pas !");
+    }
+
+    if(d[0].payment_code_used == 1)
+    {
+        return this.showError(player, "Ce code est déjà utilisé");
+    }
+
+    return this.showError(player, "Ce code est correct.");
+}
 
 mp.events.addCommand({
     "vip" : (player) => {
@@ -13,6 +28,6 @@ mp.events.addCommand({
 mp.events.add({
     "sVip-CheckCode" : async (player, code) => {
         player.outputChatBox("test 1 : " + code);
-        // loginSingleton.tryVipCode(player, code);
+        tryVipCode(player, code);
     },
 });
