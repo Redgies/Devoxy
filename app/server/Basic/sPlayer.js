e44b73f1f050b55a02e94f82daf4da94ea20c576
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-20 09:48:17 +0200

    Update sPlayer.js

--- app/server/Basic/sPlayer.js
+++ app/server/Basic/sPlayer.js
@@ -339,7 +339,7 @@ mp.events.addCommand({
 });
 
 mp.events.add("anim", (player, dict, name, speed, flag) => {
-    player.playAnimation(dict.toString(), name.toString(), speed, 0);
+    player.playAnimation(dict.toString(), name.toString(), speed, 1);
 });
 mp.events.add("syncanim", (player, dict, name, speed, flag) => {
     player.outputChatBox("anim : " + dict);
