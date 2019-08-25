5a3827ce3b50910257a5a69a83ad631dc0083c3b
Elouan MARTIN <martin.elouan.77@gmail.com>
2019-08-25 13:27:19 +0200

    Update sRafineur.js

--- app/server/Jobs/sRafineur.js
+++ app/server/Jobs/sRafineur.js
@@ -146,7 +146,7 @@ class Rafineur extends Job {
     }
 
     startWork(player) {
-        if (player.loyality < 50) return player.notify(`~r~${i18n.get('basic', 'needMoreLoyality1', player.lang)} 40 ${i18n.get('basic', 'needMoreLoyality2', player.lang)}!`);
+        // if (player.loyality < 50) return player.notify(`~r~${i18n.get('basic', 'needMoreLoyality1', player.lang)} 40 ${i18n.get('basic', 'needMoreLoyality2', player.lang)}!`);
         super.startWork(player);
         player.job.collected = 0;
         player.job = {name: this.name, collected: 0, activeTree: false};
